ddaa0e5b6cd25215c3e7d5b5668d68ca56c1af4e
Diego Inácio <diegodci@gmail.com>
2021-08-12 15:47:44 -0300

    Upload next config

---
+++ next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // ! Public config
+  publicRuntimeConfig: {
+    // * Number o crypto keys
+    N_CRYPTO_KEYS: 4,
+    // * Character set
+    SET: {
+      upper: true,
+      lower: true,
+      symbol: true,
+      number: true,
+    },
+    // * Front matter
+    AUTHOR: "Diego Inácio",
+    TITLE: "Personal Encrypted Information",
+  },
+  // ! Env Tokens
+  // * You must declare them in .env file
+  env: {
+    AUTH_TOKEN_00: process.env.AUTH_TOKEN_00,
+    AUTH_TOKEN_01: process.env.AUTH_TOKEN_01,
+    AUTH_TOKEN_02: process.env.AUTH_TOKEN_02,
+    AUTH_TOKEN_03: process.env.AUTH_TOKEN_03,
+  },
+};
